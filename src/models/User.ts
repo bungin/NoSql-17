24ade5dc49f24dc6b314bcfe5a2ff605604e7935
bungin <nimai.band@gmail.com>
2024-10-18 21:22:41 -0500

    schema prog

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,14 +3,14 @@ import { Schema, Types, model, type Document } from 'mongoose';
 interface IUser extends Document {
   username: string;
   email: string;
-  thoughts: Types.ObjectId[];
-  friends: Types.ObjectId[];
-}
+  thoughts: Schema.Types.ObjectId[];
+  friends: Schema.Types.ObjectId[];
+};
 
-const UserSchema = new Schema<IUser>({
+const userSchema = new Schema<IUser>({
     username: {
         type: String,
-        required: true,
+        required: true, 
         unique: true,
         trim: true
     },
@@ -24,4 +24,22 @@ const UserSchema = new Schema<IUser>({
         // after a dot in the domain.  regex from copilot, decipheringVVVVV
         // (/starts)(.+ any char)(@ literal)(.+ any char)(\. literal period)(.+ any char)(/close)
     },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    timestamps: true,
+    toJSON: {
+        virtuals: true
+    }
 })
