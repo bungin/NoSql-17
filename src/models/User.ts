6f98e6efde8655cd09e216e97f5dfcf36be527b9
bungin <nimai.band@gmail.com>
2024-10-18 21:06:21 -0500

    regex learnt

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,3 +6,22 @@ interface IUser extends Document {
   thoughts: Types.ObjectId[];
   friends: Types.ObjectId[];
 }
+
+const UserSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+        // this regex ensures that the string contains at least one
+        // char before and after the @ symbol, and at least one char 
+        // after a dot in the domain.  regex from copilot, decipheringVVVVV
+        // (/starts)(.+ any char)(@ literal)(.+ any char)(\. literal period)(.+ any char)(/close)
+    },
+})
