0aedd5d4e2325fd60afbbb80521da5d948ad93dd
bungin <nimai.band@gmail.com>
2024-10-19 22:27:49 -0500

    thought model

---
+++ src/models/Thought.ts
@@ -0,0 +1,43 @@
+import e from "express";
+import { Schema, model, type Document } from "mongoose";
+
+interface IThought extends Document {
+  thoughtText: string;
+  createdAt: Date;
+  username: string;
+  reactions: [];
+}
+
+const thoughtSchema = new Schema<IThought>(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model<IThought>("Thought", thoughtSchema);
+
+export default Thought;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,56 +1,58 @@
-import { Schema, model, type Document } from 'mongoose';
+import { Schema, model, type Document } from "mongoose";
 
 interface IUser extends Document {
   username: string;
   email: string;
   thoughts: Schema.Types.ObjectId[];
   friends: Schema.Types.ObjectId[];
-};
+}
 
-const userSchema = new Schema<IUser>({
+const userSchema = new Schema<IUser>(
+  {
     username: {
-        type: String,
-        required: true, 
-        unique: true,
-        trim: true,
-        maxlength: 35,
-        minlength: 2
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      maxlength: 35,
+      minlength: 2,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Please enter a valid email address']
-        // this regex ensures that the string contains at least one
-        // char before and after the @ symbol, and at least one char 
-        // after a dot in the domain.  regex from copilot, decipheringVVVVV
-        // (/starts)(.+ any char)(@ literal)(.+ any char)(\. literal period)(.+ any char)(/close)
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+      // this regex ensures that the string contains at least one
+      // char before and after the @ symbol, and at least one char
+      // after a dot in the domain.  regex from copilot, decipheringVVVVV
+      // (/starts)(.+ any char)(@ literal)(.+ any char)(\. literal period)(.+ any char)(/close)
     },
     thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
     ],
     friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
-},
-{
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
-        getters: true //do i need this? research
-    }
+      virtuals: true,
+      getters: true, //do i need this? research
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-userSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
-});
-
-const User = model<IUser>('User', userSchema);
+const User = model<IUser>("User", userSchema);
 
 export default User;
