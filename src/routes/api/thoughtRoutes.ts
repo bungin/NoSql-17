eb839c71e514d836e4604def757032ab0e436104
bungin <nimai.band@gmail.com>
2024-10-24 00:35:59 -0500

    thoughtRoutes

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} from "../../controllers/thoughtController";
+
+const router = Router();
+
+router.route("/").get(getThoughts).post(createThought);
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction)
